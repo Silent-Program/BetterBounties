e381b591a038e8e3e2febf89db42734cdcaf084b
Silent-Program <72319773+Silent-Program@users.noreply.github.com>
2022-01-06 11:10:48 -0800

    Initial commit

---
+++ src/main/java/me/silentprogram/betterbounties/BetterBounties.java
@@ -0,0 +1,16 @@
+package me.silentprogram.betterbounties;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BetterBounties extends JavaPlugin {
+	
+	@Override
+	public void onEnable(){
+		saveDefaultConfig();
+	}
+	
+	@Override
+	public void onDisable(){
+	
+	}
+}
